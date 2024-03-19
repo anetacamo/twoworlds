c5c27287f2e2a3c75a82e35b42aa306e8bb5f84b
Aneta <anc@praqma.net>
2024-03-19 09:29:41 +0100

    fixed some bugs

---
+++ src/app/components/Divider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Divider() {
+  return (
+    <section>
+      <div className='inner center'>
+        <img className='logoicon' src='images/icons/ball2.png' />
+        <img className='logoicon' src='images/icons/ball2.png' />
+        <img className='logoicon' src='images/icons/ball2.png' />
+      </div>
+    </section>
+  );
+}
