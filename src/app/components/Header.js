7ca08c6434d6b707439830026c60f42287f7e4a1
anetacamo <anetacamova@gmail.com>
2024-06-20 16:07:27 +0200

    hide top menu on scroll

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,14 +1,30 @@
 "use client";
-import React, { useState } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import Hamburger from "./Hamburger";
 import Logo from "./Logo";
 import NameSwitch from "./NameSwitch";
-import Link from "next/link";
 
 export default function Header({ player, handlePlayerChange }) {
   const [open, setOpen] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const visible = prevScrollPos > currentScrollPos || currentScrollPos < 10;
+
+      setPrevScrollPos(currentScrollPos);
+      setVisible(visible);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
   return (
-    <header>
+    <header style={{ top: visible ? "0" : "-100px", transition: "top 0.5s" }}>
       <Logo />
       <NameSwitch setPlayer={handlePlayerChange} player={player} />
       <Hamburger open={open} handleClick={() => setOpen(!open)} />
